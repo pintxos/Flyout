44b6237c919b2a9df56542de21843aeddc28b955
Jelle Versele <jelle.versele@internetarchitects.be>
2014-09-12 17:51:12 +0200

    WIP.

--- index.js
+++ index.js
@@ -22,36 +22,126 @@
 		inherit
 	) {
 
+		var Flyout, _defaults;
+
+		/* Default settings
+		----------------------------------------------- */
+		_defaults = {
+			target: undefined,
+			events: {
+				toggleEvent: 'click',
+				open: 'open.Flyout',
+				close: 'close.Flyout'
+			},
+			css: {
+				open: 'js-open'
+			}
+		};
 
 		/* Constructor
 		----------------------------------------------- */
-		var Flyout = function () {
-
+		Flyout = function (el, options) {
+			this._settings = $.extend(true, {}, _defaults, options);
+			Component.call(this, el, this._settings);
 		};
+
+		inherit(Flyout, Component);
 
 
 		/* Methods
 		----------------------------------------------- */
-
-		/**
-		 * All bootstrap logic should go here
-		 * @return {void}
-		 */
 		Flyout.prototype.init = function () {
+			this._superClass.init.call(this);
 
+			this._on(this.getTrigger(), this.getSettings().events.toggleEvent, this._onTriggerClick);
+
+			this.close();
+		};
+
+		Flyout.prototype.destroy = function () {
+			this._$target = undefined;
+			this._superClass.destroy.call(this);
+		};
+
+		Flyout.prototype.open = function () {
+
+			var activeClass;
+
+			activeClass = this.getSettings().css.open;
+
+			this.getTrigger().addClass(activeClass);
+			this.getTarget().addClass(activeClass);
+
+			this._isOpen = true;
+
+			this.getEl().trigger(this.getSettings().events.open);
+		};
+
+		Flyout.prototype.close = function () {
+
+			var activeClass;
+
+			activeClass = this.getSettings().css.open;
+
+			this.getTrigger().removeClass(activeClass);
+			this.getTarget().removeClass(activeClass);
+
+			this._isOpen = false;
+
+			this.getEl().trigger(this.getSettings().events.close);
 		};
 
 		/**
-		 * All teardown logic should go here
-		 * @return {void}
+		 * Getter for _isOpen
+		 * @return {Boolean}
 		 */
-		Flyout.prototype.destroy = function () {
+		Flyout.prototype.isOpen = function () {
+			return this._isOpen;
+		};
 
+		/**
+		 * Getter for trigger
+		 * @return {jQuery}
+		 */
+		Flyout.prototype.getTrigger = function () {
+			return this.getEl();
+		};
+
+		/**
+		 * Getter for target
+		 * @return {jQuery}
+		 */
+		Flyout.prototype.getTarget = function () {
+
+			var settingsTarget;
+
+			if(typeof this._$target === 'undefined') {
+
+				settingsTarget = this.getSettings().target;
+
+				if(typeof settingsTarget === 'undefined') {
+					this._$target = this.getTrigger().next();
+				}else{
+					this._$target = (typeof settingsTarget === 'string') ? this._query(settingsTarget) : settingsTarget;
+				}
+
+			}
+
+			return this._$target;
 		};
 
 
 		/* Event handlers
 		----------------------------------------------- */
+		Flyout.prototype._onTriggerClick = function (e) {
+			e.preventDefault();
+
+			if(this.isOpen()) {
+				this.close();
+			}else{
+				this.open();
+			}
+		};
 
 
 		/* Export
